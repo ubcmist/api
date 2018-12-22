f8162fda6acc3d3f119db035c5a97f2ce04e213f
Anmol Singh <anmolshl2@gmail.com>
2018-12-22 15:03:20 -0800

    Get biodata function implemented, more tests added, tests wrapped in promises

--- functions/tests/tests.ts
+++ functions/tests/tests.ts
@@ -2,11 +2,14 @@ import { expect } from 'chai';
 import 'mocha';
 import * as WebRequest from 'web-request';
 import * as lib from '../src/index';
+import * as Promise from 'bluebird';
 
 describe('Testing cloud functions for the BioAPI locally...', () => {
 
   const addBioURL = 'http://localhost:5000/biosignal-e309a/us-central1/onBioStore';
   const addBioURLRemote = 'https://us-central1-biosignal-e309a.cloudfunctions.net/onBioStore';
+  const getBioURL = 'http://localhost:5000/biosignal-e309a/us-central1/onBioGet';
+  const getBioURLRemote = '';
 
   it('Check data, valid data', () => {
     let newData = {
@@ -31,20 +34,26 @@ describe('Testing cloud functions for the BioAPI locally...', () => {
       },
       time: 5
     };
-    WebRequest.post(addBioURL, {json: true}, newData)
-      .then(value => {
-        console.log(value.statusCode);
-        console.log(value.message);
-        expect(value.statusCode).to.equals(200);
+    return new Promise((resolve, reject) => {
+      WebRequest.post(addBioURL, {json: true}, newData)
+        .then(value => {
+          console.log(value.statusCode);
+          console.log(value.message);
+          expect(value.statusCode).to.equals(201);
+          resolve(value)
+        })
+        .catch(err => reject(err))})
+      .then((val) => {
+        expect(val).to.equals(val);
       })
-      .catch(err => {
-        expect.fail();
+      .catch(err1 => {
+        expect.fail(err1);
       });
   });
 
   it('Test biometric store remote, new user', () => {
     let newData = {
-      userID: 7243819361,
+      userID: 9243819461,
       metrics: {
         hr: [89, 72, 77, 75,78],
         gsr: [100, 102, 105, 109, 88],
@@ -52,14 +61,20 @@ describe('Testing cloud functions for the BioAPI locally...', () => {
       },
       time: 5
     };
-    WebRequest.post(addBioURLRemote, {json: true}, newData)
-      .then(value => {
-        console.log(value.statusCode);
-        console.log(value.message);
-        expect(value.statusCode).to.equals(200);
+    return new Promise((resolve, reject) => {
+      WebRequest.post(addBioURLRemote, {json: true}, newData)
+        .then(value => {
+          console.log(value.statusCode);
+          console.log(value.message);
+          expect(value.statusCode).to.equals(201);
+          resolve(value)
+        })
+        .catch(err => reject(err))})
+      .then((val) => {
+        expect(val).to.equals(val);
       })
-      .catch(err => {
-        expect.fail();
+      .catch(err1 => {
+        expect.fail(err1);
       });
   });
 
@@ -73,20 +88,20 @@ describe('Testing cloud functions for the BioAPI locally...', () => {
       },
       time: 5
     };
-    WebRequest.post(addBioURL, {json: true}, newData)
+    WebRequest.post(addBioURLRemote, {json: true}, newData)
       .then(value1 => {
-        WebRequest.post(addBioURL, {json: true}, newData)
+        WebRequest.post(addBioURLRemote, {json: true}, newData)
           .then(value => {
             console.log(value.statusCode);
             console.log(value.message);
-            expect(value.statusCode).to.equals(200);
+            expect(value.statusCode).to.equals(205);
           })
-          .catch(err => {
-            expect.fail();
+          .catch(err1 => {
+            expect.fail(err1);
           });
       })
       .catch(err => {
-        expect.fail();
+        expect.fail(err);
       });
   });
 
@@ -100,14 +115,86 @@ describe('Testing cloud functions for the BioAPI locally...', () => {
       },
       time: 5
     };
-    WebRequest.post(addBioURLRemote, {json: true}, newData)
-      .then(value => {
-        console.log(value.statusCode);
-        console.log(value.message);
-        expect.fail();
+    return new Promise((resolve, reject) => {
+      WebRequest.post(addBioURLRemote, {json: true}, newData)
+        .then(value => {
+          expect.fail(value);
+          reject(value);
+        })
+        .catch(err => {
+          resolve(err)
+        })})
+      .then((val) => {
+        expect(val).to.equals(val);
       })
-      .catch(err => {
-        expect(err.statusCode).to.equals(400);
+      .catch(err1 => {
+        expect.fail(err1);
+      });
+  });
+
+  it("Get valid user data from firestore", () => {
+    let userQuery = {
+      userID: 6243819331
+    };
+    return new Promise((resolve, reject) => {
+      WebRequest.post(getBioURL, {json:true}, userQuery)
+        .then(value => {
+          let data = value.message['body'];
+          console.log(data);
+          expect(data['userID']).to.equals(userQuery.userID);
+          expect(value.statusCode).to.equals(200);
+          resolve(value);
+        })
+        .catch(err => {
+          reject(err);
+        })})
+      .then((val) => {
+        expect(val).to.equals(val);
+      })
+      .catch(err1 => {
+        expect.fail(err1);
+      });
+  });
+
+  it("Get invalid user data from firestore, no-existent user", () => {
+    let userQuery = {
+      userID: 4444419331
+    };
+    return new Promise((resolve, reject) => {
+      WebRequest.post(getBioURL, {json:true}, userQuery)
+        .then(value => {
+          expect.fail(value);
+          reject(value);
+        })
+        .catch(err => {
+          resolve(err);
+        })})
+      .then((val) => {
+        expect(val).to.equals(val);
+      })
+      .catch(err1 => {
+        expect.fail(err1);
+      });
+  });
+
+  it("Get invalid user data from firestore, invalid userID", () => {
+    let userQuery = {
+      userID: 44444
+    };
+    return new Promise((resolve, reject) => {
+      WebRequest.post(getBioURL, {json:true}, userQuery)
+        .then(value => {
+          expect.fail(value);
+          reject(value);
+        })
+        .catch(err => {
+          resolve(err);
+        })})
+      .then((val) => {
+        expect(val).to.equals(val);
+      })
+      .catch(err1 => {
+        expect.fail(err1);
       });
   });
 });
